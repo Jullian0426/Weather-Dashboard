c829871565a5f6a03d1bcddb8c0cd95090392b6c
Jullian0426 <jullian0426@yahoo.com>
2023-05-06 08:58:33 -0500

    Add load and save search history functions

--- js/script.js
+++ js/script.js
@@ -2,54 +2,70 @@ const searchForm = document.getElementById("search-form");
 const cityInput = document.getElementById("city-input");
 const currentWeather = document.getElementById("current-weather");
 const forecast = document.getElementById("forecast");
+const searchHistory = document.getElementById("search-history");
 
 const API_KEY = "9d667b296c3874be4a023158870a2686";
 
-searchForm.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const cityName = cityInput.value.trim();
+// Load search history on page load
+loadSearchHistory();
 
-  if (cityName) {
-    const cityData = await getCityData(cityName);
-    const weatherData = await getWeatherData(cityData.lat, cityData.lon);
-    displayCurrentWeather(weatherData);
-    displayForecast(weatherData);
-  }
+searchForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const cityName = cityInput.value.trim();
+
+    if (cityName) {
+        const cityData = await getCityData(cityName);
+        const weatherData = await getWeatherData(cityData.lat, cityData.lon);
+        displayCurrentWeather(weatherData);
+        displayForecast(weatherData);
+        saveToSearchHistory(cityData.name);
+        loadSearchHistory();
+    }
+});
+
+searchHistory.addEventListener("click", async (event) => {
+    if (event.target.matches("button")) {
+        const cityName = event.target.dataset.cityName;
+        const cityData = await getCityData(cityName);
+        const weatherData = await getWeatherData(cityData.lat, cityData.lon);
+        displayCurrentWeather(weatherData);
+        displayForecast(weatherData);
+    }
 });
 
 async function getCityData(cityName) {
-  const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
-  const data = await response.json();
-  return data[0];
-}
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`);
+    const data = await response.json();
+    return data[0];
+};
 
 async function getWeatherData(lat, lon) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
-  const data = await response.json();
-  return data;
-}
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+    const data = await response.json();
+    return data;
+};
 
 function displayCurrentWeather(data) {
-  const current = data.list[0];
-  const iconUrl = `https://openweathermap.org/img/wn/${current.weather[0].icon}.png`;
+    const current = data.list[0];
+    const iconUrl = `https://openweathermap.org/img/wn/${current.weather[0].icon}.png`;
 
-  currentWeather.innerHTML = `
+    currentWeather.innerHTML = `
     <h2>${data.city.name} (${new Date(current.dt * 1000).toLocaleDateString()})</h2>
     <img src="${iconUrl}" alt="${current.weather[0].description}">
     <p>Temperature: ${current.main.temp}°C</p>
     <p>Humidity: ${current.main.humidity}%</p>
     <p>Wind Speed: ${current.wind.speed} m/s</p>
   `;
-}
+};
 
 function displayForecast(data) {
-  const dailyData = data.list.filter((item) => item.dt_txt.endsWith("12:00:00"));
+    const dailyData = data.list.filter((item) => item.dt_txt.endsWith("12:00:00"));
 
-  forecast.innerHTML = dailyData
-    .map((day) => {
-      const iconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+    forecast.innerHTML = dailyData
+        .map((day) => {
+            const iconUrl = `https://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
 
-      return `
+            return `
         <div class="forecast-day">
           <h3>${new Date(day.dt * 1000).toLocaleDateString()}</h3>
           <img src="${iconUrl}" alt="${day.weather[0].description}">
@@ -58,6 +74,23 @@ function displayForecast(data) {
           <p>Humidity: ${day.main.humidity}%</p>
         </div>
       `;
-    })
-    .join("");
-}
+        })
+        .join("");
+};
+
+function saveToSearchHistory(cityName) {
+    const searchHistoryData = JSON.parse(localStorage.getItem("searchHistory")) || [];
+    if (!searchHistoryData.includes(cityName)) {
+        searchHistoryData.push(cityName);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistoryData));
+    }
+};
+
+function loadSearchHistory() {
+    const searchHistoryData = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+    searchHistory.innerHTML = searchHistoryData
+        .map((cityName) => `<button type="button" data-city-name="${cityName}">${cityName}</button>`)
+        .join("");
+};
+
